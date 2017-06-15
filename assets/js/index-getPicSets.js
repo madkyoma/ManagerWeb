54a1fa999e13b6e3e4b79d0a0da4740b08a86784
Crystal-jiang <934889998@qq.com>
2017-06-15 20:55:12 +0800

    2017-6-15 PicSetGet Test 1

--- assets/js/index-getPicSets.js
+++ assets/js/index-getPicSets.js
@@ -1,5 +1,4 @@
 function getPicSets(){
-  window.alert("运行了此方法getPicSets"); 
     	var params = " ";  
         var url = "http://114.115.142.42/Operation/GetPicSet";  
                 $.ajax({  
@@ -16,11 +15,66 @@ function getPicSets(){
                     if(data=="invalid!"){  
                       window.alert("非法操作！");
                     }else{
+                      //获取页面中的元素
+                      var albumArr=new Array(document.getElementById("album1"),
+                        document.getElementById("album2"),
+                        document.getElementById("album3"),
+                        document.getElementById("album4"),
+                        document.getElementById("album5"),
+                        document.getElementById("album6"),
+                        document.getElementById("album7"),
+                        document.getElementById("album8"));
+                      var albumNameArr=new Array(document.getElementById("album_name1"),
+                        document.getElementById("album_name2"),
+                        document.getElementById("album_name3"),
+                        document.getElementById("album_name4"),
+                        document.getElementById("album_name5"),
+                        document.getElementById("album_name6"),
+                        document.getElementById("album_name7"),
+                        document.getElementById("album_name8"));
+                      var albumFinishArr=new Array(document.getElementById("album_finish1"),
+                        document.getElementById("album_finish2"),
+                        document.getElementById("album_finish3"),
+                        document.getElementById("album_finish4"),
+                        document.getElementById("album_finish5"),
+                        document.getElementById("album_finish6"),
+                        document.getElementById("album_finish7"),
+                        document.getElementById("album_finish8"));
+                      var albumCoverArr=new Array(document.getElementById("album_cover1"),
+                        document.getElementById("album_cover2"),
+                        document.getElementById("album_cover3"),
+                        document.getElementById("album_cover4"),
+                        document.getElementById("album_cover5"),
+                        document.getElementById("album_cover6"),
+                        document.getElementById("album_cover7"),
+                        document.getElementById("album_cover8"));
+                      var albumIDArr=new Array(document.getElementById("album_id1"),
+                        document.getElementById("album_id2"),
+                        document.getElementById("album_id3"),
+                        document.getElementById("album_id4"),
+                        document.getElementById("album_id5"),
+                        document.getElementById("album_id6"),
+                        document.getElementById("album_id7"),
+                        document.getElementById("album_id8"));
                      	var myobj=eval(data);
                       for(var i=0;i<myobj.length;i++){
-    						        window.alert(myobj[i].Url);
-    				        		window.alert(myobj[i].PicSetName);
-    					         	window.alert(myodj[i].isFinish);
+                        //显示图集div
+                        albumArr[i].style.display="block";
+                        //更改图集封面
+                        if(myobj[i].Url!=null&&myobj[i].Url!="")
+                          albumCoverArr[i].src=myobj[i].Url;
+                        //更改图集名称
+                        if(myobj[i].PicSetName!=null&&myobj[i].PicSetName!="")
+                          albumNameArr[i].innerHTML=myobj[i].PicSetName;
+                        //更改图集标签化完成信息
+                        if(myobj[i].isFinish=="1")
+                          albumFinishArr[i].innerHTML="已完成标签化";
+                        else if(myobj[i].isFinish=="0")
+                          albumFinishArr[i].innerHTML="未完成标签化";
+                        //记录图集ID
+                        if(myobj[i].PicSetId!=null&&myobj[i].PicSetId!="")
+                          albumIDArr[i].innerHTML=myobj[i].PicSetId;
+    					         	window.alert(myobj[i].PicSetId);
 						          }
                     }
                   },
